178cb11d9fa1b4674d4e4107f3d16a68e8b9ad19
Sai Kambampati <ssv1065@gmail.com>
2021-08-19 17:41:17 -0700

    Updated prompt

--- Tests/SwiftyGPT3Tests/SwiftyGPT3Tests.swift
+++ Tests/SwiftyGPT3Tests/SwiftyGPT3Tests.swift
@@ -9,10 +9,7 @@ final class SwiftyGPT3Tests: XCTestCase {
         
         let openai = SwiftyGPT3(apiKey: "API-KEY")
         let result = try await openai.completion(
-            prompt: "user: add a box\nagft: [{\"name\": \"box1\", \"type\": \"cube\"}]\nuser: change the size to half a meter and change the color to matte black\nagft: [{\"name\": \"box1\", \"type\": \"cube\", \"size\": 0.5, \"shiny\": 0.0, \"color\": \"black\"}]\nuser: add a blue ball above the box and make it 30 centimeters in radius\nagft: [{\"name\": \"box1\", \"type\": \"cube\", \"size\": 0.5, \"shiny\": 0.0, \"color\": \"black\"}, {\"name\": \"ball1\", \"type\": \"sphere\", \"color\": \"blue\", \"position\": \"0,0,1\", \"radius\": 0.3}]\nuser: spin the ball around the box\nagft: [{\"name\": \"box1\", \"type\": \"cube\", \"size\": 0.5, \"shiny\": 0.0, \"color\": \"black\"}, {\"name\": \"ball1\", \"type\": \"sphere\", \"color\": \"blue\", \"position\": \"0,0,1\", \"radius\": 0.3, \"animation\": \"spin\", \"origin\": \"box1\"}]\nuser: add a green box next to the first box\nagft: [{\"name\": \"box1\", \"type\": \"cube\", \"size\": 0.5, \"shiny\": 0.0, \"color\": \"black\"}, {\"name\": \"ball1\", \"type\": \"sphere\", \"color\": \"blue\", \"position\": \"0,0,1\", \"radius\": 0.3, \"animation\": \"spin\", \"origin\": \"box1\"}, {\"name\": \"box2\", \"type\": \"cube\", \"color\": \"green\", \"position\": \"1,0,1\"}]\nuser: remove everything\nagft: []\nuser: add a red sphere\nagft:",
-            maxTokens: 1573,
-            temperature: 0.6,
-            topP: 1.0
+            prompt: "user: hello\nbot: Hi! How can I help you today?\nuser:"
         )
         print(result)
     }
